752941c88b4d39c1be8b96062869aa97ad031192
bhillmann <bhillmann@outlook.com>
2017-10-10 14:13:47 -0500

    Created the first conda release.

---
+++ conda-recipe/build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#$PYTHON setup.py install
+
+# Add more build steps here, if they are necessary.
+
+# See
+# http://docs.continuum.io/conda/build.html
+# for a list of environment variables that are set during the build process.
+mkdir -p $PREFIX/bin
+cp bin/* $PREFIX/bin/
+chmod +x $PREFIX/bin/*
